342cfb15c740ae5f454f79f296627a15a5969b3b
ChYuHsun <51938539+ChYuHsun@users.noreply.github.com>
2020-01-05 17:28:25 +0800

    完成對話框文字特效和放上道具

--- Unity_AI2D_B1040407_Yuhsun/Assets/Scripts/NPC.cs
+++ Unity_AI2D_B1040407_Yuhsun/Assets/Scripts/NPC.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class NPC : MonoBehaviour
 {
@@ -20,17 +21,26 @@ public class NPC : MonoBehaviour
     public string sayComplete = "You are pretty good, Man ε٩(๑> ₃ <)۶з";
 
     [Header("對話速度")]
-    public float Speed = 1.5f;
+    public float Speed = 0.1f;
 
     [Header("任務相關")]
     public bool complete;
-    public int comtPlayer;
+    public int countPlayer;
     public int countFinish = 10;
 
     [Header("介面")]
     public GameObject objCanvas;
     public Text textSay;
     #endregion
+
+    public AudioClip soundSay;
+
+    private AudioSource aud;
+
+    private void Start()
+    {
+        aud = GetComponent<AudioSource>();
+    }
 
     /// <summary>
     /// 呼叫；處發對話框
@@ -56,18 +66,53 @@ public class NPC : MonoBehaviour
     }
 
     /// <summary>
-    /// 對話：打字效果
+    /// 開啟對話：打字效果
     /// </summary>
     private void Say()
     {
         objCanvas.SetActive(true);
-        textSay.text = sayStart;
+        StopAllCoroutines();
+
+        if(countPlayer >= countFinish) _state = state.complete;
+         
+        //判斷任務狀態
+        switch (_state)
+        {
+            case state.normal:
+                StartCoroutine(ShowDialog(sayStart));        //開始對話
+                _state = state.notComplete;
+                break;
+            case state.notComplete:
+                StartCoroutine(ShowDialog(sayNotComplete));    //未完成對話
+                break;
+            case state.complete:
+                StartCoroutine(ShowDialog(sayComplete));       //完成對話
+                break;
+        }
     }
+
+    /// <summary>
+    /// 打字效果
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ShowDialog(string say)
+    {
+        textSay.text = "";
+
+        for (int i = 0; i < say.Length; i++)       //迴圈跑對話
+        {
+            textSay.text += say[i].ToString();      //累加文字
+            aud.PlayOneShot(soundSay, 1.5f);        //播放聲音
+            yield return new WaitForSeconds(Speed);     //等待
+        }
+    }
+
     /// <summary>
     /// 離開對話
     /// </summary>
     private void SayClose()
     {
+        StopAllCoroutines();
         objCanvas.SetActive(false);
     }
 

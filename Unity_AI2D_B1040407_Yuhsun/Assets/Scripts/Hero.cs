b6ac7dacee654a5a2807ae590fdafd9cefa74bf8
ChYuHsun <51938539+ChYuHsun@users.noreply.github.com>
2020-01-05 15:06:49 +0800

    角色移動

---
+++ Unity_AI2D_B1040407_Yuhsun/Assets/Scripts/Hero.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public int speed = 10;          //移動數值
+    public float jump = 2.0f;       //跳躍數值
+    public string heroName = "Hero";
+    public bool pass = false;
+
+    private Rigidbody2D r2d;        //角色移動宣告
+    //private Transform tra;          //角色旋轉宣告
+
+    private void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+        //tra = GetComponent<Transform>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) transform.eulerAngles = new Vector3(0, 0, 0);
+        if (Input.GetKeyDown(KeyCode.A)) transform.eulerAngles = new Vector3(0, 180, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        ///
+        r2d.AddForce(new Vector2(speed * Input.GetAxis("Horizontal"), 0));
+    }
+}

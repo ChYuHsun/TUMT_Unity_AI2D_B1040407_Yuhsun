a2e66c2f21230d07e0d5eb08f520e49aa00cb13d
ChYuHsun <51938539+ChYuHsun@users.noreply.github.com>
2020-01-05 19:18:59 +0800

    繪製射線

---
+++ Unity_AI2D_B1040407_Yuhsun/Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("移動速度"),Range(0, 100)]
+    public float speed = 10.0f;
+    [Header("傷害"), Range(0, 100)]
+    public float damege = 20f;
+
+    public Transform checkPoint;
+
+    private Rigidbody2D r2d;
+
+    private void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(checkPoint.position, -checkPoint.up * 3);
+    }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move()  
+    {
+        r2d.AddForce(new Vector2(-speed, 0));
+    }
+
+    /// <summary>
+    /// 追蹤玩家
+    /// </summary>
+    private void Track()
+    {
+
+    }
+}
